344a45255fd0ff214c11f97ece7acaef1ad9f8de
likachedia <likachedia@gmail.com>
2022-10-11 20:50:15 +0400

    add moduls to the project

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const slugify = require('slugify');
+
+// import { replaceTemplate } from './modules.js/replaceTemplates';
+const replaceTemplate = require('./modules/replaceTemplates');
 // blocking, syncronouse code
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -24,26 +29,28 @@ const url = require('url');
 
 ////////////////////
 //// SERVER
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+// const replaceTemplate = (temp, product) => {
+//     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+//     output = output.replace(/{%IMAGE%}/g, product.image);
+//     output = output.replace(/{%PRICE%}/g, product.price);
+//     output = output.replace(/{%ID%}/g, product.id);
+//     output = output.replace(/{%DESCRIPTION%}/g, product.description);
+//     output = output.replace(/{%FROM%}/g, product.from);
+//     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+//     output = output.replace(/{%QUANTITY%}/g, product.quantity);
 
-   if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-   return output;
+//    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+//    return output;
 
-}
+// }
 const templOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const templCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const templProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(ele => slugify(ele.productName, {lower: true}));
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
